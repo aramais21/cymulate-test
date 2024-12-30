1af3af9cbb2d9c00bcb9926e9752eb6efb57cdf4
aramais21 <aramaisyeghiazaryan@gmail.com>
2024-12-31 03:22:33 +0400

    * Changed front-end from web-app to react

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/phishing-front-end',
+  cacheDir: '../../node_modules/.vite/apps/frontend',
   server: {
     port: 4200,
     host: 'localhost'
@@ -14,28 +15,17 @@ export default defineConfig({
     port: 4300,
     host: 'localhost'
   },
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
   build: {
-    outDir: '../../dist/apps/phishing-front-end',
+    outDir: '../../dist/apps/frontend',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true
-    }
-  },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/phishing-front-end',
-      provider: 'v8'
     }
   }
 })
